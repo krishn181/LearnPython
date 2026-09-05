c5b2523fb9a15681f153c3d9eb199ee9faa669ec
Anish <singhkrishn704@gmail.com>
2026-09-05 18:18:56 +0530

    Authentication

--- Channels/MyProject3/MyProject3/asgi.py
+++ Channels/MyProject3/MyProject3/asgi.py
@@ -4,13 +4,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import app.routing
+from channels.auth import AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyProject2.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
 
-    "websocket": URLRouter(
+    "websocket": AuthMiddlewareStack(
+    URLRouter(
         app.routing.websocket_urlpatterns
-    ),
+    ),)
 })
 
--- Channels/MyProject3/app/consumers.py
+++ Channels/MyProject3/app/consumers.py
@@ -2,6 +2,7 @@ from channels.consumer import AsyncConsumer, SyncConsumer
 from channels.exceptions import StopConsumer
 import json
 from .models import Chat, Group
+from channels.auth import AuthMiddlewareStack
 from asgiref.sync import async_to_sync
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
@@ -32,15 +33,20 @@ class MySyncConsumer(SyncConsumer):
             group = Group.objects.get(name=self.group_name)
         except Group.DoesNotExist:
             pass
-        chat = Chat(content = message, group = group)
-        chat.save()
-        async_to_sync(self.channel_layer.group_send)(
-        self.group_name,
-        {
-            'type': 'chat.message',
-            'message': event['text'],
-        }
-        )
+        if self.scope['user'].is_authenticated:
+            chat = Chat(content = message, group = group)
+            chat.save()
+            async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+                {
+                    'type': 'chat.message',
+                    'message': event['text'],
+                }        )
+        else:
+            self.send({
+                'type':'websocket.send',
+                'text':json.dumps({"msg":"login required"}),
+                })
     def chat_message(self, event):
         print("Event.....",event['message'])
         data = json.loads(event['message'])
